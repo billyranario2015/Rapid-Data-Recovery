42e3a8d49989a6e2839831d26f305d1bcc1e04b6
Billy Ranario <billy.ranario@must.edu.ph>
2015-06-01 23:21:15 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rapid Data Recovery</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="author" content="" />
+	<meta content="index, follow, all" name="robots" />
+    <meta name="description" content="Rapid Data Recovery Experts" />
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css">
+</head>
+<body>
+	<header>
+		<section class="navbar bs-docs-nav" id="top" role="banner">
+			<div class="container" id="main-menu-container">
+				<div class="navbar-header">
+					<button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a href="" class="navbar-brand">
+						<label class="head-title">
+							<span class="text-orange">rapid</span><span class="text-dblue">data</span><span class="text-orange">recovery</span>							
+						</label>
+						<span class="head-tag">Cheapest Data Recovery in the UK - Guaranteed!</span>
+					</a>
+				</div>
+				<nav class="navbar-collapse bs-navbar-collapse collapse" id="main-menu-wrapper" aria-expanded="false" style="height: 1px;">
+					<ul class="nav navbar-nav navbar-right" id="main-menu">
+						<li>
+							<a href="#">Home</a>
+						</li>
+						<li>
+							<a href="#">Data Recovery Prices</a>
+						</li>
+						<li>
+							<a href="#">Data Recovery Process</a>
+						</li>
+						<li>
+							<a href="#">Why Choose Us</a>
+						</li>
+						<li>
+							<a href="#">What Our Clients Say</a>
+						</li>
+						<li>
+							<a href="#">Guarantee</a>
+						</li>
+						<li>
+							<a href="#">Contact Us</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</section>				
+	</header>
+	<section class="container" id="main-container">
+		<div class="main-content">
+			<section class="col-md-9 banner">
+				
+			</section>
+			<section class="col-md-3">
+				
+			</section>
+		</div>
+	</section>
+	<footer>
+		
+	</footer>
+</body>
+</html>
